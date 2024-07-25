7f26e82fecca0f60032d32a37941708ed2eecc33
Joseph Eiba <josepheiba@icloud.com>
2024-07-25 19:23:47 +0100

    commit db

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+
+def pascal_triangle(n):
+    list_list = []
+    if n > 0:
+        list_list.append([1])
+        for i in range(1, n):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = list_list[i - 1][j] + list_list[i - 1][j - 1]
+            list_list.append(row)
+    return list_list
+        
